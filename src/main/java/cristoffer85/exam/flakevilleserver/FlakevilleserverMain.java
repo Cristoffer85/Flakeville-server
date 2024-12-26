a7238695be1fb3bc236bb1cf8545c97c21479ca6
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-12-26 15:12:50 +0100

    Test update application update to cred

--- src/main/java/cristoffer85/exam/flakevilleserver/FlakevilleserverMain.java
+++ src/main/java/cristoffer85/exam/flakevilleserver/FlakevilleserverMain.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlakevilleserverMain {
 
     public static void main(String[] args) {
+        System.out.println("RABBITMQ_HOST: " + System.getenv("RABBITMQ_HOST"));
+        System.out.println("RABBITMQ_USERNAME: " + System.getenv("RABBITMQ_USERNAME"));
+        System.out.println("RABBITMQ_PASSWORD: " + System.getenv("RABBITMQ_PASSWORD"));
         SpringApplication.run(FlakevilleserverMain.class, args);
     }
 }
