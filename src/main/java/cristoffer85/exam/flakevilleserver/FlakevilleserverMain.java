03e4e8c81b5aa22a933b6dfb9c1d76ed61bb9578
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-12-27 18:58:32 +0100

    clarifications and small namechanges in application.properties

--- src/main/java/cristoffer85/exam/flakevilleserver/FlakevilleserverMain.java
+++ src/main/java/cristoffer85/exam/flakevilleserver/FlakevilleserverMain.java
@@ -1,6 +1,6 @@
 package cristoffer85.exam.flakevilleserver;
 
-        //import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
