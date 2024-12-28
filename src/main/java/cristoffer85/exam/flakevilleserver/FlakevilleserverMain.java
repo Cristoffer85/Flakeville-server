ae9258871b86ffbcd415fcdcc62a610887a0aade
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2024-12-28 20:40:28 +0100

    redid server+local development, again.

--- src/main/java/cristoffer85/exam/flakevilleserver/FlakevilleserverMain.java
+++ src/main/java/cristoffer85/exam/flakevilleserver/FlakevilleserverMain.java
@@ -1,6 +1,5 @@
 package cristoffer85.exam.flakevilleserver;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlakevilleserverMain {
 
     public static void main(String[] args) {
-
-        // Make spring boot application able to load .env file for local development
-        Dotenv dotenv = Dotenv.configure().load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-        //----------------------------------------------
-
+        
         SpringApplication.run(FlakevilleserverMain.class, args);
     }
 }
